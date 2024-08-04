989ef22c61214265e1f12f8b441032e95dfd254e
Tuaannn <vantuan.97.pt@gmail.com>
2024-08-04 22:02:16 +0700

    fixdirection

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ document.querySelector(".logo").onclick = function () {
     window.location.href = "./index.html";
 };
 
+document.querySelector(".mobile-header-logo").onclick = function () {
+    window.location.href = "./index.html";
+};
+
 document.querySelector(".footer__logo").onclick = function () {
     window.location.href = "#";
 };
