500342f1fa72f2999f70b005349aac89a5e8e39e
Tuaannn <vantuan.97.pt@gmail.com>
2024-08-04 21:54:13 +0700

    blog

---
+++ main.js
@@ -0,0 +1,7 @@
+document.querySelector(".logo").onclick = function () {
+    window.location.href = "./index.html";
+};
+
+document.querySelector(".footer__logo").onclick = function () {
+    window.location.href = "#";
+};
